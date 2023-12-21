cf41b7e028bd6808e4b0daf0a34f9a69535fab15
Nham24 <nham24@Nham24s-MacBook-Air-3.local>
2023-12-21 14:03:45 +0700

    change_fin

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -7,7 +7,7 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table') }}
+-- {{ config(materialized='table') }}
 
 
 -- SQL model to build a hash column based on the values of this record
