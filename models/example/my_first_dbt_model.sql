639972946b5e072854ab9a153903d21759275599
Nham24 <nham24@Nham24s-MacBook-Air-3.local>
2023-12-21 15:43:48 +0700

    sql_final

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -13,7 +13,7 @@
 -- SQL model to build a hash column based on the values of this record
 -- depends_on: __dbt__cte__sheet1_ab2
 
-create table "postgres".public."store_update"
+create table "postgres".public.store_update
   as (
     select _airbyte_emitted_at,
         (current_timestamp at time zone 'utc')::timestamp as _airbyte_normalized_at,
@@ -24,5 +24,5 @@ create table "postgres".public."store_update"
         cast(jsonb_extract_path_text(_airbyte_data, 'store') as varchar) as store,
         cast(jsonb_extract_path_text(_airbyte_data, 'admin_name') as varchar) as admin_name
         -- cast(jsonb_extract_path_text(_airbyte_data, 'service') as varchar) as service_,
-    from "postgres".public."._airbyte_raw_sheet1"
+    from "postgres".public._airbyte_raw_sheet1
 );
