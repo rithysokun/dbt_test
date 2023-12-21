19fc27abd7541f1369068f7140c434e9bf0f8dc7
Nham24 <nham24@Nham24s-MacBook-Air-3.local>
2023-12-21 13:15:09 +0700

    test2

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -18,13 +18,12 @@ create table "postgres".public."sheet1__dbt_tmp"
     select
         _airbyte_emitted_at,
         (current_timestamp at time zone 'utc')::timestamp as _airbyte_normalized_at,
-
         cast(jsonb_extract_path_text(_airbyte_data, 'city') as varchar) as city,
         cast(jsonb_extract_path_text(_airbyte_data, 'commune') as varchar) as commune,
         cast(jsonb_extract_path_text(_airbyte_data, 'district') as varchar) as district,
-        cast(jsonb_extract_path_text(_airbyte_data, 'id') as varchar) asi d,
+        cast(jsonb_extract_path_text(_airbyte_data, 'id') as varchar) as id,
         cast(jsonb_extract_path_text(_airbyte_data, 'store') as varchar) as store,
-        cast(jsonb_extract_path_text(_airbyte_data, 'admin_name') as varchar) as admin_name,
-        cast(jsonb_extract_path_text(_airbyte_data, 'service') as varchar) as service,
+        cast(jsonb_extract_path_text(_airbyte_data, 'admin_name') as varchar) as admin_name
+        -- cast(jsonb_extract_path_text(_airbyte_data, 'service') as varchar) as service_,
     from "postgres".public."sheet1__dbt_tmp"
 );
