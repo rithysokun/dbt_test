a526f84105eb17f2bc9b99a1b932fb2cd06a9628
Nham24 <nham24@Nham24s-MacBook-Air-3.local>
2023-12-21 15:31:16 +0700

    store_update-sql

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -13,16 +13,16 @@
 -- SQL model to build a hash column based on the values of this record
 -- depends_on: __dbt__cte__sheet1_ab2
 
-create table "postgres"."public"."sheet1__dbt_tmp"
+create table "postgres".public."store_update"
   as (
     select _airbyte_emitted_at,
         (current_timestamp at time zone 'utc')::timestamp as _airbyte_normalized_at,
+        cast(jsonb_extract_path_text(_airbyte_data, 'id') as int) as id,
         cast(jsonb_extract_path_text(_airbyte_data, 'city') as varchar) as city,
         cast(jsonb_extract_path_text(_airbyte_data, 'commune') as varchar) as commune,
         cast(jsonb_extract_path_text(_airbyte_data, 'district') as varchar) as district,
-        cast(jsonb_extract_path_text(_airbyte_data, 'id') as varchar) as id,
         cast(jsonb_extract_path_text(_airbyte_data, 'store') as varchar) as store,
         cast(jsonb_extract_path_text(_airbyte_data, 'admin_name') as varchar) as admin_name
         -- cast(jsonb_extract_path_text(_airbyte_data, 'service') as varchar) as service_,
-    from "postgres".public."sheet1__dbt_tmp"
+    from "postgres".public."._airbyte_raw_sheet1"
 );
